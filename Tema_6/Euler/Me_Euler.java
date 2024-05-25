20c67d5c5145cf03740fc9959518169e9ffec0c5
CristianCHsx <cris.tian.04232@gmail.com>
2024-05-25 16:19:56 -0600

    Update - v1_Programa - Arreglar

--- Tema_6/Euler/Me_Euler.java
+++ Tema_6/Euler/Me_Euler.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 
 public class Me_Euler{
 
-    public float edo(float x, float y){
-        
-        return x;
+    public static float edo(float x, float y){
+        return (float)y-x;
     }
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+        
+        float x=0,xn,yn,xparad,h;
 
+        System.out.print("Ingresa el valor de x: ");
+        xn = sc.nextFloat();
+        System.out.print("Ingresa el valor de y: ");
+        yn = sc.nextFloat();
+        System.out.print("Ingresa el valor de x final: ");
+        xparad = sc.nextFloat();
+        System.out.print("Ingresa el numero de pasos (h): ");
+        h = sc.nextFloat();
+    
+        do{
+            x+=1;
+            xn+=h;
+            System.out.println(x+"<->"+xn);
+            yn=yn+(h*edo(xn,yn));
+            System.out.println(x+"<->"+yn);
 
+        }while(xn!=xparad);
+        System.out.println("El resultado de la ecuacion diferencial ordinaria es: "+yn);
         
     }
 }
