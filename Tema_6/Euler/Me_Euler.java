391fbafc91c041e8655920dd707bac22b575eddd
CristianCHsx <cris.tian.04232@gmail.com>
2024-05-26 05:51:28 -0600

    Update - Codigo Java Euler - Tema6

--- Tema_6/Euler/Me_Euler.java
+++ Tema_6/Euler/Me_Euler.java
@@ -1,35 +1,39 @@
 package Tema_6.Euler;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 
 public class Me_Euler{
 
-    public static float edo(float x, float y){
-        return (float)y-x;
+    public static double edo(double x, double y){
+        return 2*x*y;
     }
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
+        DecimalFormat de = new DecimalFormat("#.00000");
         
-        float x=0,xn,yn,xparad,h;
+        
+        double x0=0,y0=0,xf=0,h=0,yn,x=0,n=0;
 
-        System.out.print("Ingresa el valor de x: ");
-        xn = sc.nextFloat();
-        System.out.print("Ingresa el valor de y: ");
-        yn = sc.nextFloat();
-        System.out.print("Ingresa el valor de x final: ");
-        xparad = sc.nextFloat();
-        System.out.print("Ingresa el numero de pasos (h): ");
-        h = sc.nextFloat();
-    
+        System.out.println("Ingresa el valor de x inferior: ");
+        x0 = sc.nextDouble();
+        System.out.println("Ingresa el valor de y: ");
+        y0 = sc.nextDouble();
+        System.out.println("Ingresa el valor de x final: ");
+        xf = sc.nextDouble();
+        System.out.println("Ingresa la cantidad de pasos: ");
+        n = sc.nextDouble();
+
+        h=(xf-x0)/n;
+
         do{
+            System.out.println(x);
+            yn=y0+(h*edo(x0, y0));
+            x0+=h;
+            y0=yn;
             x+=1;
-            xn+=h;
-            System.out.println(x+"<->"+xn);
-            yn=yn+(h*edo(xn,yn));
-            System.out.println(x+"<->"+yn);
+        }while(x!=n);
 
-        }while(xn!=xparad);
-        System.out.println("El resultado de la ecuacion diferencial ordinaria es: "+yn);
-        
+        System.out.println("En la iteracion "+x+"--"+de.format(x0)+" "+ de.format(yn));
     }
 }
